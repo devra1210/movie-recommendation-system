b49e18b16b73cb0115d1532645a0c9c25ee950d9
Hrishitha Mulakkayala <hrishitha.m@gmail.com>
2025-02-24 16:28:55 -0800

    created dataframes

--- recommender.py
+++ recommender.py
@@ -1,4 +1,18 @@
 import numpy as np
 import pandas as pd
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import os
+
+movies_df = pd.read_csv('data/tmdb_5000_movies.csv')
+credits_df = pd.read_csv('data/tmdb_5000_credits.csv')
+
+print(movies_df.head(2))
+print(credits_df.head(2))
+
+print(movies_df.shape)
+print(credits_df.shape)
+
+movies_df = pd.merge(movies_df, credits_df, on='title', how='outer')
+print(movies_df)
+
+
